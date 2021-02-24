10bc229451a8a8470284dde742e5450f07767b21
Adelin <Adebota02@gmail.com>
2021-02-24 12:25:24 +0100

    Actualizacion

--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -7,30 +7,6 @@ import AgentesTIA.*;
 public class Main {
 
 	
-	public static int crearMenu() {
-		Scanner leer = new Scanner(System.in);
-		int opcion=0;
-		do {
-			System.out.println("1. Ver todos los agentes con su información");
-			System.out.println("2. Ver los agentes que ganen más de una cantidad X");
-			System.out.println("3. Dar de alta un nuevo piso");
-			System.out.println("4. Dar de alta una nueva arma");
-			System.out.println("5. Dar de alta un nuevo agente");
-			System.out.println("6. Entcriptar toda la información");
-			System.out.println("7. Desencriptar toda la información");
-			System.out.println("8. Salir");
-			
-			try {
-				opcion = leer.nextInt();
-			} catch (Exception e) {
-				System.out.println("Escribe un número de la lista de opciones");
-				leer = new Scanner(System.in);
-				opcion = 0;
-			}	
-		} while (opcion>8 || opcion<1);
-		
-		return opcion;
-	}
 	
 	public static void main(String[] args) {
 
@@ -43,9 +19,9 @@ public class Main {
 		vAgente[3] = new OO7("Braithwaite", 29, "C/Barcelona nº 9", 800, 5);
 		
 		
-		switch (crearMenu()) {
+		switch (Utilidades.Utilidades.crearMenu()) {
 		case 1:
-			System.out.println(mostrarListaAgentes());
+			Utilidades.Utilidades.mostrarListaAgentes(vAgente);
 			break;
 		case 2:
 			
